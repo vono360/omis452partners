3d893c4410015cecae3767088840901c8dca8086
vono360 <54908211+vono360@users.noreply.github.com>
2019-10-01 00:14:12 -0500

    Delete insertProject.sql

--- Exercises/EX03/insertProject.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO Project VALUES('2017 Q3 Production Plan', 'Production', 100.00, '10-May-17', '15-Jun-17');
-INSERT INTO Project VALUES('2017 Q3 Marketing Plan', 'Sales and Marketing', 135.00, '10-May-17', '15-Jun-17');
-INSERT INTO Project VALUES('2017 Q3 Portfolio Analysis', 'Finance', 120.00, '05-Jul-17', '25-Jul-17');
-INSERT INTO Project VALUES('2017 Q3 Tax Preparation', 'Accounting', 145.00, '10-Aug-17', '15-Oct-17');
-INSERT INTO Project VALUES('2017 Q4 Production Plan', 'Production', 100.00, '10-Aug-17', '15-Sep-17');
-INSERT INTO Project VALUES('2017 Q4 Marketing Plan', 'Sales and Marketing', 135.00, '10-Aug-17', '15-Sep-17');
-INSERT INTO Project(ProjectName, Department, MaxHours, StartDate) VALUES('2017 Q4 Portfolio Analysis', 'Finance', 140.00, '05-Oct-17');
