804a08138fe5a1b8a588a446177858cc66a0472a
vono360 <54908211+vono360@users.noreply.github.com>
2019-10-08 13:09:17 -0500

    Add files via upload

---
+++ Exercises/EX04/Query Statements.sql
@@ -0,0 +1,115 @@
+/*********************************************************************/
+/*                      ExerciseEX04 Queries                         */
+/*                    Austin Nelson and Amelia                       */
+/*                                                                   */
+/*********************************************************************/
+
+/* *** SQL-Query-CH03-1 Austin and Amelia *** */
+SELECT ProjectID, ProjectName, Department, MaxHours, StartDate, EndDate
+FROM PROJECT;
+
+/* *** SQL-Query-CH03-2 Austin and Amelia *** */
+SELECT * 
+FROM PROJECT;
+
+/* *** SQL-Query-CH03-3 Austin and Amelia *** */
+SELECT ProjectName, Department, MaxHours
+FROM PROJECT;
+
+/* *** SQL-Query-CH03-4 Austin and Amelia *** */
+SELECT ProjectName, MaxHours, Department
+FROM PROJECT;
+
+/* *** SQL-Query-CH03-5 Austin and Amelia *** */
+SELECT Department
+FROM PROJECT;
+
+/* *** SQL-Query-CH03-6 Austin and Amelia *** */
+SELECT DISTINCT Department
+FROM PROJECT;
+
+/* *** SQL-Query-CH03-7 Austin and Amelia *** */
+SELECT * 
+FROM PROJECT
+WHERE Department= 'Finance';
+
+/* *** SQL-Query-CH03-8 Austin and Amelia *** */
+SELECT * 
+FROM PROJECT
+WHERE StartDate = '05/10/2017';
+
+/* *** SQL-Query-CH03-9 Austin and Amelia *** */
+SELECT *
+FROM PROJECT
+WHERE MaxHours > 135;
+
+/* *** SQL-Query-CH03-10 Austin and Amelia *** */
+SELECT FirstName, LastName, Department, OfficePhone
+FROM EMPLOYEE
+WHERE Department = 'Accounting';
+
+/* *** SQL-Query-CH03-11 Austin and Amelia *** */
+SELECT FirstName, LastName, Department, OfficePhone
+FROM EMPLOYEE
+ORDER BY Department;
+
+/* *** SQL-Query-CH03-12 Austin and Amelia *** */
+SELECT FirstName, LastName, Department, OfficePhone
+FROM EMPLOYEE
+ORDER BY Department DESC;
+
+/* *** SQL-Query-CH03-13 Austin and Amelia *** */
+SELECT FirstName, LastName, Department, OfficePhone
+FROM EMPLOYEE
+ORDER BY Department DESC, LastName ASC;
+
+/* *** SQL-Query-CH03-14 Austin and Amelia *** */
+SELECT FirstName, LastName, Department, OfficePhone
+FROM EMPLOYEE
+WHERE Department = 'Accounting'
+    AND OfficePhone = '360-285-8430';
+
+/* *** SQL-Query-CH03-15 Austin and Amelia *** */
+SELECT FirstName, LastName, Department, OfficePhone
+FROM EMPLOYEE
+WHERE Department = 'Accounting'
+    OR  OfficePhone = '360-285-8430';
+
+/* *** SQL-Query-CH03-16 Austin and Amelia *** */
+SELECT FirstName, LastName, Department, OfficePhone
+FROM EMPLOYEE
+WHERE Department = 'Accounting'
+    AND NOT OfficePhone = '360-285-8430';
+
+/* *** SQL-Query-CH03-17 Austin and Amelia *** */
+SELECT FirstName, LastName, Department, OfficePhone
+FROM EMPLOYEE
+WHERE Department IN ('Administration', 'Finance', 'Accounting');
+
+/* *** SQL-Query-CH03-18 Austin and Amelia *** */
+SELECT FirstName, LastName, Department, OfficePhone
+FROM EMPLOYEE
+WHERE Department NOT IN ('Administration', 'Finance', 'Accounting');
+
+/* *** SQL-Query-CH03-19 Austin and Amelia *** */
+SELECT FirstName, LastName, Department, OfficePhone
+FROM EMPLOYEE
+WHERE EmployeeNumber >=2
+    AND EmployeeNumber <= 5;
+
+/* *** SQL-Query-CH03-20 Austin and Amelia *** */
+SELECT FirstName, LastName, Department, OfficePhone
+FROM EMPLOYEE
+WHERE EmployeeNumber BETWEEN 2 AND 5;
+
+/* *** SQL-Query-CH03-21 Austin and Amelia *** */
+SELECT * 
+FROM PROJECT
+WHERE ProjectName LIKE '2017 Q_ Portfolio Analysis';
+
+/* *** SQL-Query-CH03-22 Austin and Amelia *** */
+SELECT * 
+FROM EMPLOYEE
+WHERE OfficePhone LIKE '360-287-88__';
+
+/* *** SQL-Query-CH03-23 Austin and Amelia *** */
