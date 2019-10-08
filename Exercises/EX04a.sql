153a70d909e14b64b2f9b0c40d29a19ab25c9698
vono360 <54908211+vono360@users.noreply.github.com>
2019-10-08 10:26:46 -0500

    Create EX04a.sql

---
+++ Exercises/EX04a.sql
@@ -0,0 +1,9 @@
+/*********************************************************************/
+/*                      ExerciseEX04 Queries                         */
+/*                    Austin Nelson and Amelia                       */
+/*                                                                   */
+/*********************************************************************/
+
+/* *** SQL-Query-CH03-1 Austin and Amelia *** */
+SELECT ProjectID, ProjectName, Department, MaxHours, StartDate, EndDate
+FROM PROJECT;
