29d5429b0fcc70330ac9b63d8df3ab43a2899d4e
vono360 <54908211+vono360@users.noreply.github.com>
2019-10-31 14:33:40 -0500

    Update SubQuery-Pattern-Amelia&Austin.sql
    Double Not Exists

--- Exercises/EX06a/SubQuery-Pattern-Amelia&Austin.sql
+++ Exercises/EX06a/SubQuery-Pattern-Amelia&Austin.sql
@@ -3,7 +3,9 @@ SELECT DISTINCT ProjectID
 FROM `project`
 WHERE NOT EXISTS
 	(SELECT *
-		FROM project
-WHERE NOT EXISTS (SELECT *
-FROM assignment
-;
+		FROM `project` JOIN assignment 
+     		ON assignment.ProjectID = `project`.`ProjectID`
+     	WHERE project.Department = 'Finance'
+     	AND NOT EXISTS( SELECT *
+                       FROM project
+                       WHERE EmployeeNumber = 1))
