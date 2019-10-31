b1b784fd8fe764e906d9442011fa778f9ac30f69
vono360 <54908211+vono360@users.noreply.github.com>
2019-10-31 14:24:22 -0500

    Update SubQuery-Pattern-Amelia&Austin.sql

--- Exercises/EX06a/SubQuery-Pattern-Amelia&Austin.sql
+++ Exercises/EX06a/SubQuery-Pattern-Amelia&Austin.sql
@@ -1,1 +1,9 @@
-
+/*      Amelia and Austin Subqueries      */
+SELECT DISTINCT ProjectID
+FROM `project`
+WHERE NOT EXISTS
+	(SELECT *
+		FROM project
+WHERE NOT EXISTS (SELECT *
+FROM assignment
+;
